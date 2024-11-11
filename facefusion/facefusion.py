97da3543f3618115ea43cabb8a81e5e821fa611a
Eduard Moraru <eduardmoraru@Eduards-MBP.attlocal.net>
2024-11-11 03:05:05 -0500

    Fixed imports and circular import issues

--- facefusion/facefusion.py
+++ facefusion/facefusion.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
-
 import os
+import gradio as gr
+from facefusion.uis.layouts.default import create_gradio_ui  # Correct path to your Gradio UI function
 
-os.environ['OMP_NUM_THREADS'] = '1'
+# Get the port from the environment variable provided by Render, default to 7860 if not set
+port = os.getenv("PORT", 7860)
 
-from facefusion import core
+# Create the Gradio interface using the function
+gradio_app = create_gradio_ui()
 
-if __name__ == '__main__':
-	core.cli()
+# Launch the Gradio app on the appropriate port
+gradio_app.launch(server_name="0.0.0.0", server_port=port, share=False)  # share=True if you need public link
