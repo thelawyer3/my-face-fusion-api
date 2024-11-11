cd9ac7c986d53a0d3a23ea974fcaed570ae8995c
Eduard Moraru <eduardmoraru@Eduards-MBP.attlocal.net>
2024-11-11 03:32:54 -0500

    Updated the FaceFusion setup and fixed port configurations

--- facefusion/facefusion.py
+++ facefusion/facefusion.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python3
+
 import os
-import gradio as gr
-from facefusion.uis.layouts.default import create_gradio_ui  # Correct path to your Gradio UI function
 
-# Get the port from the environment variable provided by Render, default to 7860 if not set
-port = os.getenv("PORT", 7860)
+os.environ['OMP_NUM_THREADS'] = '1'
 
-# Create the Gradio interface using the function
-gradio_app = create_gradio_ui()
+from facefusion import core
 
-# Launch the Gradio app on the appropriate port
-gradio_app.launch(server_name="0.0.0.0", server_port=port, share=False)  # share=True if you need public link
+if __name__ == '__main__':
+	core.cli()
--- facefusion/facefusion/uis/__init__.py
+++
@@ -1,6 +0,0 @@
-# facefusion/facefusion/uis/__init__.py
-
-from facefusion.uis.layouts.default import launch_gradio_ui
-
-def get_launch_gradio_ui():
-    return launch_gradio_ui
